3cdea420e2478275cbaee8a1ca6f0c892996e824
JuanCruzValencia <jcvalencia@ismt.edu.ar>
2022-11-22 22:06:41 -0300

    tests

--- desafio_entregable_2/app.js
+++ desafio_entregable_2/app.js
@@ -3,16 +3,15 @@ const fs = require("fs");
 
 // Uso de estructura el desafio anterior
 class ProductManager {
-  #products;
-
   constructor() {
-    this.#products = [];
     this.path = "./data/DB.json";
   }
 
-  getProductId = () => {
-    const index = this.#products.length;
-    const id = index > 0 ? index + 1 : 1;
+  getProductId = async () => {
+    const productsCopy = await fs.promises.readFile(this.path, "utf-8");
+    const productsCopyObj = JSON.parse(productsCopy);
+    const index = productsCopyObj.length;
+    const id = index === undefined ? index + 1 : 1;
     return id;
   };
 
@@ -29,15 +28,18 @@ class ProductManager {
     );
   };
 
-  verifyCode = (productCode) => {
-    const productsCopy = [...this.#products];
-    const isInDb = productsCopy.some((product) => product.code === productCode);
+  verifyCode = async (productCode) => {
+    const productsCopy = await fs.promises.readFile(this.path, "utf-8");
+    const productsCopyObj = JSON.parse(productsCopy);
+    const isInDb = productsCopyObj.some(
+      (product) => product.code === productCode
+    );
     return isInDb;
   };
 
   addProduct = async (title, description, price, thumbnail, code, stock) => {
-    const id = this.getProductId();
-
+    const id = this.getProductId().then((id) => id);
+    console.log(id);
     const newProduct = {
       id,
       title,
@@ -53,9 +55,8 @@ class ProductManager {
     if (this.verifyCode(newProduct.code))
       console.log("This product code already exist is the data base");
 
-    this.#products.push(newProduct);
     // creo el archivo donde voy a guardar los datos de mi array de productos
-    await fs.promises.writeFile(this.path, JSON.stringify(this.#products));
+    await fs.promises.writeFile(this.path, JSON.stringify([newProduct]));
     console.log("TEST: PRODUCTO AGREGDO CON EXITO");
   };
 
@@ -65,7 +66,7 @@ class ProductManager {
     try {
       const resolve = await fs.promises.readFile(this.path, "utf-8");
       if (resolve.length === 0) throw new Error();
-      return JSON.parse(resolve);
+      return resolve;
     } catch (error) {
       console.log("Array vacio");
     }
@@ -85,19 +86,26 @@ class ProductManager {
   };
 
   // funcion para actualizar productos de mi archivo
-  updateProduct = async (updatedProduct) => {
-    // primero elimino el producto anterior con el id de mi producto actualizado
-    this.deleteProduct(updatedProduct.id);
-    // despues guardo mi nuevo producto en mi array
-    const updatedArray = this.#products.push(updatedProduct);
-    // sobre escribo mi archivo con mi producto actualizado
-    await fs.promises.writeFile(this.path, JSON.stringify(updatedArray));
+  updateProduct = async (productId, objUpdated) => {
+    const productsCopy = await fs.promises.readFile(this.path, "utf-8");
+    const productsCopyObj = JSON.parse(productsCopy);
+    const productToUpdate = productsCopyObj.find(
+      (product) => product.id === productId
+    );
+    const filteredProducts = productsCopyObj.filter(
+      (product) => product.id !== productId
+    );
+    const prodcutUpdated = { id: productToUpdate.id, ...objUpdated };
+    filteredProducts.push(prodcutUpdated);
+    await fs.promises.writeFile(this.path, JSON.stringify(filteredProducts));
+    console.log(filteredProducts);
   };
 
   // funcion para eliminar prodcutos de mi archivo
   deleteProduct = async (productId) => {
-    const productsCopy = [...this.#products];
-    const filteredProducts = productsCopy.filter(
+    const productsCopy = await fs.promises.readFile(this.path, "utf-8");
+    const productsCopyObj = JSON.parse(productsCopy);
+    const filteredProducts = productsCopyObj.filter(
       (product) => product.id !== productId
     );
     await fs.promises.writeFile(this.path, JSON.stringify(filteredProducts));
@@ -106,32 +114,50 @@ class ProductManager {
 
 // TESTS
 
-// Creando una instancia de la clase Product Manager
+// A - Creando una instancia de la clase Product Manager
 const valencia = new ProductManager();
 
-// Se invoca a getProducts() devuelve productos | error y mensaje
-valencia
-  .getProducts()
-  .then((products) =>
-    console.log(`Primer test: ${products ? products : "NO HAY PRODUCTOS"}`)
-  );
+// B - Se invoca a getProducts() devuelve productos | error y mensaje
+// valencia.getProducts().then((products) => {
+//   if (products !== undefined) {
+//     const result = JSON.parse(products);
+//     console.log(result);
+//   }
+// });
 
-// Se agrega un nuevo producto
+// C - Se agrega un nuevo producto
 
-valencia
-  .addProduct(
-    "Producto de prueba",
-    "Este es un producto de prueba",
-    250,
-    "No hay imagen",
-    "abc123",
-    50
-  )
-  .then(() => {
-    // Se invoca a getProducts() nuevamente con el prodcuto ya ingresado en el array
-    valencia
-      .getProducts()
-      .then((products) =>
-        console.log(`Segundo test: ${products ? products : "NO HAY PRODUCTOS"}`)
-      );
-  });
+valencia.addProduct(
+  "Producto de prueba",
+  "Este es un producto de prueba",
+  250,
+  "No hay imagen",
+  "abc123",
+  50
+);
+
+// D - Se invoca a getProducts() nuevamente con el prodcuto ya ingresado en el array
+// valencia.getProducts().then((products) => {
+//   const result = JSON.parse(products);
+//   console.log(result);
+// });
+
+// E - Se invoca a la funcion getProductById para verificar que exista en al DB
+// valencia.getProductById(1).then((product) => {
+//   console.log(product);
+// });
+
+// F - Se invoca a la funcion updateProduct con objeto completo y las modificaciones
+// const newObj = {
+//   title: "Producto de prueba actualizado",
+//   description: "Este es el producto actualizado",
+//   price: 250,
+//   thumbnail: "No hay imagen",
+//   code: "abc123",
+//   stock: 50,
+// };
+
+// valencia.updateProduct(1, newObj);
+
+// G - Se invoca a la funcion deleteProduct con el id del objeto a eliminar
+// valencia.deleteProduct(1);
