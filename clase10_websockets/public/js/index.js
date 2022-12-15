de13e62888384d8f6c47dfe8a1c4f1b0e93ca296
JuanCruzValencia <jcvalencia@ismt.edu.ar>
2022-12-15 09:52:10 -0300

    "deleted clase 10"

--- clase10_websockets/public/js/index.js
+++
@@ -1,9 +0,0 @@
-const socket = io();
-
-// emitiendo un mensaje desde el cliente
-socket.emit("message", "Enviando el primer mensaje con socket");
-
-// recibiendo el mensaje del servidor
-socket.on("para uno", (data) => {
-  console.log(data);
-});
