766c930ba30a2af292d26c1dc5e6260c00dcd235
Juan Cruz Valencia Vernengo <jcvalencia@ismt.edu.ar>
2023-04-11 12:36:23 -0300

    working on tests for post method for api/products

--- desafio_entregable_12/src/products/tests/products.test.js
+++ desafio_entregable_12/src/products/tests/products.test.js
@@ -8,40 +8,62 @@ const requester = supertest(process.env.BASE_URL);
 
 describe("Testing products endpoint", () => {
   describe("/api/products", () => {
-    it("Get should return an status 200", async () => {
+    const newProduct = {};
+
+    it("GET should return status 200", async () => {
       const { status } = await requester.get("/api/products");
 
       expect(status).to.exist.and.to.be.equal(200);
     });
 
-    it("Get should return an array", async () => {
+    it("GET should return an array", async () => {
       const { _body } = await requester.get("/api/products");
 
       expect(_body.payload).to.be.an("array").that.is.not.empty;
     });
+
+    it("POST a new product should return status 200 if all required properties was sended", async () => {
+      const { status } = requester.post("/api/products").send(newProduct);
+
+      expect(status).to.exist.and.to.be.equal(200);
+    });
+
+    it("POST a new product should return status 400 if any of the required properties was undefined", async () => {
+      const { status } = requester.post("/api/products").send(newProduct);
+
+      expect(status).to.exist.and.to.be.equal(200);
+    });
+
+    it("POST must save the product in DB with a new _id property", async () => {
+      const { status } = requester.post("/api/products").send(newProduct);
+
+      expect(status).to.exist.and.to.be.equal(200);
+    });
   });
 
   describe("/api/products/pid", () => {
+    const pid = "63bf146e58e7baa835ee6870";
+
     it("GET should return status 200 if pid is defined", async () => {
-      const { status } = await requester.get("/api/products/");
+      const { status } = await requester.get(`/api/products/${pid}`);
 
       expect(status).to.exist.and.to.be.equal(200);
     });
 
     it("GET payload should return an object is pid is defined", async () => {
-      const { _body } = await requester.get("/api/products");
+      const { _body } = await requester.get(`/api/products/${pid}`);
 
       expect(_body.payload).to.exist.and.to.be.an("object");
     });
 
-    it("GET payload obect must have property _id", async () => {
-      const { _body } = await requester.get("/api/products");
+    it("GET payload object must have property _id", async () => {
+      const { _body } = await requester.get(`/api/products/${pid}`);
 
-      expect(_body).to.haveOwnProperty("_id");
+      expect(_body.payload).to.haveOwnProperty("_id");
     });
 
     it("GET should return status 400 if pid is not defined", async () => {
-      const { status } = await requester.get("/api/products/");
+      const { status } = await requester.get(`/api/products/123456`);
 
       expect(status).to.exist.and.to.be.equal(400);
     });
